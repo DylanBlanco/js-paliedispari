7f55b3ac87b7296952db2cfa041443f1f60cf2b1
DylanBlanco <dylanblanco1198@gmail.com>
2024-07-22 02:34:25 +0200

    PARI E DISPARI - Determina il vincitore

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -1,9 +1,9 @@
 //scegli paro o disparo
 let choose = prompt(`scegli pari o dispari`);
-console.log (`pari o dispari:`,choose);
-
+console.log (`pari o dispari:`,choose, typeof choose);
+//valori autorizzati
 const autorizzati = ["pari", "dispari", "paro", "disparo"];
-console.log (autorizzati);
+console.log (autorizzati, typeof autorizzati);
 if (choose == autorizzati[0]) {
     console.log (`autorizzato:`, choose);
 }
@@ -23,8 +23,8 @@ else {
 
 //chiedi numero da 1 a 5
 let num = prompt(`Inserisci numero da 1 a 5:`);
-console.log (`num scelto:`, num);
-
+console.log (`num scelto:`, num, typeof num);
+//valori autorizzati
 if (num > 5) {
     console.log (`num non valido:`, num);
     alert (`Numero non valido. Inserisci numero da 1 a 5.`);
@@ -37,9 +37,36 @@ else if (num < 1) {
 //genera numRandom da 1 a 5 usando funzione
 function numRandom (random) {
     //genera num. random da 1 a 5
-    let casual = Math.floor(Math.random() * 5) + 1;
+    let casual = Math.floor(Math.random() * 5 )+ 1;
     console.log (`num. casuale generato:`, casual);
-
     //return
     return casual;
+}
+
+//somma dei num e numRandom / pari o dispari?
+function somma (num, numRandom) {
+    //somma valori
+    let somma = num + numRandom;
+    if(somma % 2 == 0) {
+        console.log (`Il risultato è pari:`, somma);
+        return true;
+    }
+    else {
+        console.log (`Il risultato è dispari:`, somma);
+        return false;
+    }
+}
+
+// Determina il vincitore
+if (choose == "pari" && somma) {
+    console.log (`Il risultato è pari. Hai Vinto`);
+    alert (`HAI VINTO`);
+}
+else if (choose == "dispari" && !somma) {
+    console.log (`Il risultato è dispari. Hai Vinto`);
+    alert (`HAI VINTO`);
+}
+else {
+    console.log (`Hai Perso`);
+    alert (`HAI PERSO`);
 }
